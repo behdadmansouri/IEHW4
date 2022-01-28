7e8bec64fff48e8532bf799ebf564f115f4ecd1e
Behdad Mansouri <behdadaspacey2099@gmail.com>
2022-01-28 12:46:50 +0330

    Admin movie update and delete work too

--- main.py
+++ main.py
@@ -47,24 +47,24 @@ def token_required(f):
 @app.route("/admin/movie", methods=['POST'])
 @token_required
 def admin_movie_insert(role):
-    try:
-        if role < 1:
-            return make_response({'message': 'Not Authorized'}, 401)
-        content = request.json
-        name = content.get('name')
-        description = content.get('description')
-        if (name is not None and description is not None and
-                type(name) == str and type(description) == str):
-            db_query('INSERT INTO MOVIE (NAME DESCRIPTION) VALUES ("%s", "%s")' % (name, description))
+    # try:
+    if role < 1:
+        return make_response({'message': 'Not Authorized'}, 401)
+    content = request.json
+    name = content.get('name')
+    description = content.get('description')
+    if (name is not None and description is not None and
+            type(name) == str and type(description) == str):
+        db_query('INSERT INTO MOVIE (NAME DESCRIPTION) VALUES ("%s", "%s")' % (name, description))
 
-        # Bad Request
-        else:
-            return make_response({'message': 'Bad Request'}, 400)
+    # Bad Request
+    else:
+        return make_response({'message': 'Bad Request'}, 400)
 
-        # SUCCESS
-        return make_response({'message': 'ok'}, 204)
-    except Exception:
-        return make_response({'message': 'internal server error'}, 500)
+    # SUCCESS
+    return make_response({'message': 'ok'}, 204)
+    # except Exception:
+    #     return make_response({'message': 'internal server error'}, 500)
 
 
 @app.route("/admin/movie/<movie_id>", methods=['PUT', 'DELETE'])
@@ -80,14 +80,15 @@ def admin_movie_update_delete(role, movie_id):
             description = content.get('description')
             if (name is not None and description is not None and
                     type(name) == str and type(description) == str):
-                db_query('UPDATE MOVIE SET NAME="%s", DESCRIPTION="%s" WHERE ID=%i' % (name, description, movie_id))
+                db_query('UPDATE MOVIE SET NAME="%s", DESCRIPTION="%s" WHERE ID=%i'
+                         % (name, description, int(movie_id)))
             else:
                 return make_response({'message': 'Bad Request'}, 400)
         elif request.method == 'DELETE':
-            db_query('DELETE FROM MOVIE WHERE ID=%i' % movie_id)
+            db_query('DELETE FROM MOVIE WHERE ID=%i' % int(movie_id))
 
         # SUCCESS
-        return make_response(204, {'message': 'ok'})
+        return make_response({'message': 'ok'}, 204)
     except Exception:
         return make_response({'message': 'internal server error'}, 500)
 
@@ -103,12 +104,12 @@ def admin_comment_update_delete(role, comment_id):
             content = request.json
             approved = content.get('approved')
             if approved is not None and type(approved) == bool:
-                db_query('UPDATE COMMENTS SET APPROVED={0} WHERE COMMENT_ID={1}'.format(approved, comment_id))
+                db_query('UPDATE COMMENTS SET APPROVED={0} WHERE ID={1}'.format(approved, comment_id))
             else:
                 return make_response({'message': 'Bad Request'}, 400)
 
         elif request.method == 'DELETE':
-            db_query('DELETE FROM COMMENTS WHERE COMMENT_ID=%i' % comment_id)
+            db_query('DELETE FROM COMMENTS WHERE ID=%i' % int(comment_id))
 
         # SUCCESS
         return make_response({'message': 'ok'}, 204)
@@ -119,25 +120,25 @@ def admin_comment_update_delete(role, comment_id):
 @app.route("/user/comment", methods=['POST'])
 @token_required
 def user_comment(role):
-    # try:
-    if role < 0:
-        return make_response({'message': 'Not Authorized'}, 401)
+    try:
+        if role < 0:
+            return make_response({'message': 'Not Authorized'}, 401)
 
-    user_id = jwt.decode(request.headers['jwt_token'], app.config['SECRET_KEY'], algorithms=['HS256'])['user_id']
-    content = request.json
-    movie_id = content.get('movie_id')
-    comment_body = content.get('comment_body')
-    if (movie_id is not None and comment_body is not None and
-            type(movie_id) == int and type(comment_body) == str):
-        db_query('INSERT INTO COMMENTS (USER_ID, MOVIE_ID, COMMENT, CREATEDAT)'
-                 ' VALUES (%i, %i, "%s", datetime(\'now\'))' % (user_id, movie_id, comment_body))
+        user_id = jwt.decode(request.headers['jwt_token'], app.config['SECRET_KEY'], algorithms=['HS256'])['user_id']
+        content = request.json
+        movie_id = content.get('movie_id')
+        comment_body = content.get('comment_body')
+        if (movie_id is not None and comment_body is not None and
+                type(movie_id) == int and type(comment_body) == str):
+            db_query('INSERT INTO COMMENTS (USER_ID, MOVIE_ID, COMMENT, CREATEDAT)'
+                     ' VALUES (%i, %i, "%s", datetime(\'now\'))' % (user_id, movie_id, comment_body))
 
-        # SUCCESS
-        return make_response({'message': 'ok'}, 204)
-    else:
-        return make_response({'message': 'Bad Request'}, 400)
-    # except Exception:
-    #     return make_response({'message': 'internal server error'}, 500)
+            # SUCCESS
+            return make_response({'message': 'ok'}, 204)
+        else:
+            return make_response({'message': 'Bad Request'}, 400)
+    except Exception:
+        return make_response({'message': 'internal server error'}, 500)
 
 
 @app.route("/user/vote", methods=['POST'])
@@ -146,42 +147,42 @@ def user_vote(role):
     try:
         if role < 0:
             return make_response({'message': 'Not Authorized'}, 401)
-        user_id = jwt.decode(request.headers['jwt_token'])['user_id']
+        user_id = jwt.decode(request.headers['jwt_token'], app.config['SECRET_KEY'], algorithms=['HS256'])['user_id']
         content = request.json
         movie_id = content.get('movie_id')
         vote = content.get('vote')
 
         if (movie_id is not None and vote is not None and
                 type(movie_id) == int and type(vote) == int):
-            db_query('INSERT INTO COMMENTS (USER_ID, MOVIE_ID, RATING) VALUES (%i, %i, %d)'
-                     % (user_id, movie_id, vote / 10))
+            db_query('INSERT INTO COMMENTS (USER_ID, MOVIE_ID, COMMENT) VALUES (%i, %i, %d)'
+                     % (int(user_id), int(movie_id), float(vote) / 10))
+            return make_response({'message': 'ok'}, 204)
         else:
             return make_response({'message': 'Bad Request'}, 400)
-        # SUCCESS
-        return make_response({'message': 'ok'}, 204)
+    # SUCCESS
     except Exception:
         return make_response({'message': 'internal server error'}, 500)
 
 
 @app.route("/comments", methods=['GET'])
 def comments():
-    # try:
-    args = request.args
-    movie_id = args.get('movie_id')
-    if movie_id:
-        query = 'SELECT C.ID, U.USERNAME, C.COMMENT ' \
-                'FROM COMMENTS AS C, USER AS U, MOVIE AS M ' \
-                'WHERE C.USER_ID=U.ID AND M.ID=C.MOVIE_ID ' \
-                'AND M.ID=%i AND C.APPROVED=1;' % int(movie_id)  # TODO use _Join_ instead of _Where_
-        comment_list = []
-        for row in db_query(query):
-            comment_list.append({'author': row[0], 'body': row[1]})
+    try:
+        args = request.args
+        movie_id = args.get('movie_id')
+        if movie_id:
+            query = 'SELECT C.ID, U.USERNAME, C.COMMENT ' \
+                    'FROM COMMENTS AS C, USER AS U, MOVIE AS M ' \
+                    'WHERE C.USER_ID=U.ID AND M.ID=C.MOVIE_ID ' \
+                    'AND M.ID=%i AND C.APPROVED=TRUE;' % int(movie_id)  # TODO use _Join_ instead of _Where_
+            comment_list = []
+            for row in db_query(query):
+                comment_list.append({'author': row[0], 'author': row[1], 'the comment': row[2]})
 
-        return make_response(jsonify({'comment_list': comment_list}), 200)
-    else:
-        return make_response({'message': 'Bad Request'}, 400)
-    # except Exception:
-    #     return make_response({'message': 'internal server error'}, 500)
+            return make_response(jsonify({'comment_list': comment_list}), 200)
+        else:
+            return make_response({'message': 'Bad Request'}, 400)
+    except Exception:
+        return make_response({'message': 'internal server error'}, 500)
 
 @app.route("/movies", methods=['GET'])
 def public_list_movies():
@@ -206,8 +207,7 @@ def public_movie(movie_id):
             break
         return make_response(jsonify(movie_info), 200)
     except Exception:
-        # return make_response({'message': 'internal server error'}, 500)
-        pass
+        return make_response({'message': 'internal server error'}, 500)
 
 
 app.run(debug=True)
